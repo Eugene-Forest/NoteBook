c100ca78bda7c915997399635de485e25e93ec94
Eugene-Forest <1244303915@qq.com>
2021-09-29 09:58:46 +0800

    测试conf.py

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ source_suffix = {
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+# master_doc = 'index'
 
 # General information about the project.
 project = u'NoteBook'
